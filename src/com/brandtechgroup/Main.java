b0e5acb95bc1cbed1260c9878e6ef1ea0b96bf94
Camilo Lucena <camilolucena88@gmail.com>
2020-10-28 08:22:13 +0100

    Initial commit

---
+++ src/com/brandtechgroup/Main.java
@@ -0,0 +1,8 @@
+package com.brandtechgroup;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World and Welcome to our first Java Lesson");
+    }
+}
